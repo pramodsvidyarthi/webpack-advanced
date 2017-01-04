cbb8a34fef39f49d712a42418c478101cc42203d
pramod <pramodsvidyarthi@github.com>
2017-01-04 19:18:58 +0530

    added test for navigation component using enzyme and tape

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router'
 
 export default () => {
   return (
-    <div className='clearfix'><Row>
-      <Col lg='6' md='6'><Link to=''>Home</Link></Col>
-      <Col lg='6' md='6'><Link to='/about'>About</Link></Col>
-    </Row>
+    <div className='clearfix'>
+     <Row>
+        <Col lg='6' md='6'><Link to=''>Home</Link></Col>
+        <Col lg='6' md='6'><Link to='/about'>About</Link></Col>
+      </Row>
       <Row>
         <Col lg='6' md='6'><Link to='/contact'>Contact</Link></Col>
         <Col lg='6' md='6'><Link to='/portfolio'>Portfolio</Link></Col>
