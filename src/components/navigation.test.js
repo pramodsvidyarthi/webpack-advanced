6d546854db2b4d922da6874872f6cc5d81cb40af
pramod <pramodsvidyarthi@github.com>
2017-01-05 15:16:16 +0530

    testing the navigation component

--- src/components/navigation.test.js
+++ src/components/navigation.test.js
@@ -5,9 +5,14 @@ import { Row, Col } from 'reactstrap'
 
 import Navigation from './navigation'
 
-test('Layout has two rows and four columns', t => {
-  t.plan(2)
+// use shallow when testing presentational components and use shallow when testing container components
+// use static for testing static text
+
+test('Layout has two rows and four columns and is wrapped inside a clearfix class', t => {
+  t.plan(4)
   const wrapper = shallow(<Navigation />)
+  t.equal(wrapper.type(), 'div', 'Its wrapped in a div')
+  t.ok(wrapper.hasClass('clearfix'), 'It has a class name called clearfix')
   t.equals(wrapper.find(Row).length, 2, 'There are two rows')
   t.equals(wrapper.find(Col).length, 4, 'There are four columns')
   t.end()
@@ -23,3 +28,4 @@ test('There are four links and the link names are correct', t => {
   t.equals(wrapper.find('a').eq(3).text(), 'Portfolio', 'Fourth link is Portfolio')
   t.end()
 })
+
