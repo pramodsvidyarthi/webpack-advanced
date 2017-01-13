f1decd6580b3e3716dd139428880742fa2c4053a
pramod <pramodsvidyarthi@gmail.com>
2017-01-13 11:07:51 +0530

    updated tests to use jsdom for full dom rendering

--- src/components/about.test.js
+++ src/components/about.test.js
@@ -1,7 +1,14 @@
 import test from 'tape';
 import React from 'react';
-import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
 import About from './about';
+import jsdom from 'jsdom';
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+global.document = doc;
+global.window = win;
 
 test('it displays the appropriate hello message', t => {
   const wrapper = shallow(<About />);
@@ -11,3 +18,13 @@ test('it displays the appropriate hello message', t => {
   t.end();
 });
 
+test('it calls componentDidMount', t => {
+  sinon.spy(console, 'log');
+  const wrapper = mount(<About />);
+  const message = 'Hello there...this line is not touched';
+  t.ok(wrapper.length, 'it has been rendered');
+  t.equals(console.log.callCount, 1, 'it logs the message');
+  t.ok(console.log.calledWith(message), 'it has been called with the same args');
+  console.log.restore();
+  t.end();
+});
